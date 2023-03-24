02b63640b9fe8b0b681f6f341eb358fb3ef364ba
Barışcan Güngör <bariscangungor@outlook.com>
2023-03-24 18:46:59 +0300

    Initial commit

---
+++ src/main/java/com/bariscan/sigorta_hatirlatici/OtomobilSigortaOdemeHatirlaticisiApplication.java
@@ -0,0 +1,13 @@
+package com.bariscan.sigorta_hatirlatici;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtomobilSigortaOdemeHatirlaticisiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtomobilSigortaOdemeHatirlaticisiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bariscan/sigorta_hatirlatici/OtomobilSigortaOdemeHatirlaticisiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bariscan.sigorta_hatirlatici;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtomobilSigortaOdemeHatirlaticisiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
