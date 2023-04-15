ddd8678287d6ca87cbf0967007819b35690ac71d
Barışcan Güngör <bariscangungor@outlook.com>
2023-04-15 19:34:48 +0300

    userDto was created

---
+++ src/main/java/com/bariscan/sigorta_hatirlatici/dto/UserDto.java
@@ -0,0 +1,28 @@
+package com.bariscan.sigorta_hatirlatici.dto;
+
+import lombok.*;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Setter
+@Getter
+public class UserDto {
+    @NotBlank
+    @Size(min = 2, max = 20)
+    private String firstName;
+    @NotBlank
+    @Size(min = 2,max = 20)
+    private String lastName;
+    @Email(regexp = "^[\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$")
+    @NotBlank
+    @Size(max = 50)
+    private String email;
+    @NotBlank
+    @Size(min = 8, max = 100)
+    private String password;
+}
